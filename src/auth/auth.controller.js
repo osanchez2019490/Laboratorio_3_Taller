11bf3b61fea98025bbe3cfb9c36d543cd5236453
osanchez-2019490 <osanchez-2019490@kinal.edu.gt>
2024-02-26 17:21:48 -0600

    Agregando auth y haciendo proceso de token

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,7 @@
+import bcrypt from 'bcrypt'; 
+import Admin from '../admin/admin.model.js';
+import { generateJWT } from '../helpers/generate-jwt.js';
+
+export const login = async (req, res) => {
+    
+}
